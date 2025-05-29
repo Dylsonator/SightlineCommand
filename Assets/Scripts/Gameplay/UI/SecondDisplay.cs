b40721c647b3b100759ed7c930e8ec4baccc96c2
DylanFliski <Dylanfliski@gmail.com>
2025-05-29 21:58:49 +0100

    Stuff

--- Assets/Scripts/Gameplay/UI/SecondDisplay.cs
+++ Assets/Scripts/Gameplay/UI/SecondDisplay.cs
@@ -7,9 +7,15 @@ public class SecondDisplay : MonoBehaviour
 {
     private void Awake()
     {
+        int count = 0;
         foreach (var Disp in Display.displays)
         {
+            count++;
             Disp.Activate(Disp.systemWidth, Disp.systemHeight, 60);
+            if (count == 2)
+            {
+                break;
+            }
         }
     }
 }
